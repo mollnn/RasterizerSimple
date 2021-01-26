4200d5ae2055a0f34d0f086d063ba3d6c61f3c6d
mollnn <1084918073@qq.com>
2021-01-26 20:44:56 +0800

    feat image

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,25 @@
 
 #include "vec.hpp"
 #include "mat.hpp"
+#include "color.hpp"
+#include "image.hpp"
+#include "light.hpp"
+#include "material.hpp"
 
 int main(int argc, char *argv[])
 {
     std::cout << "Hello" << std::endl;
+
+    // A test to gen TGA image
+    TGAImage image(200, 200, 3);
+
+    for (int i = 0; i < 200; i++)
+        for (int j = 0; j < 200; j++)
+        {
+            image.set(i, j, color3(i * 0.005, j * 0.005, 0));
+        }
+
+    image.flip_vertically();
+
+    image.write_tga_file("output.tga");
 }
