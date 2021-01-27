3c957c4bf0c51cb78c7595af55f6874439adb3a0
mollnn <1084918073@qq.com>
2021-01-27 20:05:02 +0800

    feat MVP

--- src/image.hpp
+++ src/image.hpp
@@ -26,7 +26,59 @@ struct Image
         this->reallocate();
     }
 
-    
+    void Set(int x, int y, color3 color)
+    {
+        assert(0 <= x && x < size_x);
+        assert(0 <= y && y < size_y);
+        buffer[x][y] = color;
+    }
+
+    color3 Get(int x, int y)
+    {
+        return buffer[x][y];
+    }
+
+    TGAImage ToTGAImage()
+    {
+        TGAImage tga_image(size_x, size_y, 3);
+        for (int i = 0; i < size_x; i++)
+        {
+            for (int j = 0; j < size_y; j++)
+            {
+                tga_image.set(i, j, TGAColor(buffer[i][j].x * 255, buffer[i][j].y * 255, buffer[i][j].z * 255));
+            }
+        }
+        return tga_image;
+    }
+
+    void FromTGAImage(const TGAImage &tga_image)
+    {
+        size_x = tga_image.get_width();
+        size_y = tga_image.get_height();
+        reallocate();
+        for (int i = 0; i < size_x; i++)
+        {
+            for (int j = 0; j < size_y; j++)
+            {
+                auto tga_color = tga_image.get(i, j);
+                buffer[i][j].x = tga_color[2] / 255.;
+                buffer[i][j].y = tga_color[1] / 255.;
+                buffer[i][j].z = tga_color[0] / 255.;
+            }
+        }
+    }
+
+    void WriteToTGA(std::string filename)
+    {
+        ToTGAImage().write_tga_file(filename);
+    }
+
+    void ReadFromTGA(std::string filename)
+    {
+        TGAImage tga_image;
+        tga_image.read_tga_file(filename);
+        FromTGAImage(tga_image);
+    }
 };
 
 #endif
--- src/renderer.hpp
+++ src/renderer.hpp
@@ -12,9 +12,48 @@
 
 struct Renderer
 {
-    void Render(const Scene &scene, const Camera& camera, TGAImage& image)
+    void Render(const Scene &scene, const Camera &camera, Image &image)
     {
-        
+        vec3 cam_pos = camera.position;
+        vec3 cam_gaze = camera.gaze;
+        vec3 cam_top = camera.top;
+        vec3 cam_gtc = cross(camera.gaze, camera.top);
+
+        mat<4, 4> mat_t_view = {(vec4){1, 0, 0, -cam_pos.x},
+                                {0, 1, 0, -cam_pos.y},
+                                {0, 0, 1, -cam_pos.z},
+                                {0, 0, 0, 1}};
+        mat<4, 4> mat_r_view = {(vec4){cam_gtc.x, cam_gtc.y, cam_gtc.z, 0},
+                                {cam_top.x, cam_top.y, cam_top.z, 0},
+                                {-cam_gaze.x, -cam_gaze.y, -cam_gaze.z, 0},
+                                {0, 0, 0, 1}};
+        mat<4, 4> mat_view = mat_r_view * mat_t_view;
+
+        double box_n = 0;
+        double box_f = -5;
+        double box_l = -2;
+        double box_r = 2;
+        double box_b = -2;
+        double box_t = 2;
+
+        mat<4, 4> mat_ortho_scale = {(vec4){2 / (box_r - box_l), 0, 0, 0},
+                                     {0, 2 / (box_t - box_b), 0, 0},
+                                     {0, 0, 2 / (box_n - box_f), 0},
+                                     {0, 0, 0, 1}};
+        mat<4, 4> mat_ortho_shift = {(vec4){1, 0, 0, -(box_r + box_l) / 2},
+                                     {0, 1, 0, -(box_t + box_b) / 2},
+                                     {0, 0, 1, -(box_n + box_f) / 2},
+                                     {0, 0, 0, 1}};
+
+        mat<4, 4> mat_persp2ortho = {(vec4){box_n, 0, 0, 0},
+                                     {0, box_n, 0, 0},
+                                     {0, 0, box_n + box_f, -box_n * box_f},
+                                     {0, 0, 1, 0}};
+
+        mat<4, 4> mat_ortho = mat_ortho_scale * mat_ortho_shift;
+        mat<4, 4> mat_persp = mat_ortho * mat_persp2ortho;
+
+        // continue!
     }
 };
 
